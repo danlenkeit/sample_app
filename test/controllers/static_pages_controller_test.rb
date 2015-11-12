bb68be0a8f12f7ac85b88dbc5c22457d619d8a14
danlenkeit <danlenkeit@gmail.com>
2015-11-11 23:05:53 -0500

    Added tests/first refactor

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,11 +4,19 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
+    assert_select "title", "HOME|DLEN CHILL"
   end
 
   test "should get help" do
     get :help
     assert_response :success
+    assert_select "title", "HELP|DLEN CHILL"
+  end
+
+  test "should get about" do
+    get :about
+    assert_response :success
+    assert_select "title", "ABOUT|DLEN CHILL"
   end
 
 end
