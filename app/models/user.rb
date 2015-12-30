4fc0f321c582bac8dfdc3720726f36c39a390f70
danlenkeit <danlenkeit@gmail.com>
2015-12-29 23:39:54 -0500

    User model, secure password, whoops

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,6 @@ class User < ActiveRecord::Base
 	before_save { self.email = email.downcase }
 
 	validates :name, presence: true, length: { maximum: 50 }
-
 	validates :password, length: { minimum: 6 }
 
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
