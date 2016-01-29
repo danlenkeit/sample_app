7feef466f5b9756eb2aeb4b35a31883fdd749d0d
danlenkeit <danlenkeit@gmail.com>
2016-01-29 00:53:54 -0500

    Flash test

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -8,6 +8,7 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
   	post login_path, session: { email: "", password: "" }
   	assert_template 'sessions/new'
   	assert_not flash.empty?
+ 	assert_select "div.alert-danger", count: 1
   	get root_path
   	assert flash.empty?
   end
